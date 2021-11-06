089214741316b2b786a35abca708b565dff42233
Devesh Reddy <rdverse@alchemy.local>
2021-11-06 15:23:30 -0500

    add google oauth

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const express = require('express');
 // import express from 'express' es25 modules
 
-const app = express();
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/',  (req,res)=>  {
-res.send({ hi : "sdfa"});
-});
+const app = express();
+passport.use(new googleStrategy)
+
+
+// app.get('/',  (req,res)=>  {
+// res.send({ hi : "sdfa"});
+// });
 
 const PORT =  process.env.PORT || 5005;
 app.listen(PORT, () => {console.log(`APp is up and running at PORT : ${PORT}`);});
