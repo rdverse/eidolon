c8c29dd2714078e31b6ca13d78c5ba5d7f3a4fe2
Devesh Reddy <rdverse@alchemy.local>
2021-11-06 14:59:20 -0500

    heroku integration

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const app = express();
 app.get('/',  (req,res)=>  {
 res.send({ hi : "sdfa"});
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+const PORT =  process.env.PORT || 5005;
+app.listen(PORT, () => {console.log(`APp is up and running at PORT : ${PORT}`);});
