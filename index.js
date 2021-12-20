3a5380ab514934dfcc7b511697152680ea5f2cbb
Devesh Reddy <rdverse@alchemy.krisunni.com>
2021-12-20 13:49:38 -0500

    fix index path

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if(process.NODE_ENV=="production"){
     // send the client index html
     const path = require('path');
     app.get('*', (req,res)=>{
-res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
     });
 }
 
